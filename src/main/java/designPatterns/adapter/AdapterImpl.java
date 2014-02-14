6b40b769a63ad541832e4ce278f9799252a0c3a6
tom1410 <thomas.legrand@axellience.com>
2014-02-14 11:02:43 +0100

    Update AdapterImpl.java

--- src/main/java/designPatterns/adapter/AdapterImpl.java
+++ src/main/java/designPatterns/adapter/AdapterImpl.java
@@ -9,34 +9,7 @@ package designPatterns.adapter;
 
 public class AdapterImpl implements Adapter
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
-	public Adaptee adaptee;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public AdapterImpl(){
-		super();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public abstract void request() {
-		// TODO : to implement	
-	}
 	
 }
 
