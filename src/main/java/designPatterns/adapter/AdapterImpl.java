98cdf357a967ee8f2a1f0a9ec34500a23418c1d1
genmymodel <help@genmymodel.com>
2014-02-14 11:04:11 +0100

    commit by genmymodel from www.genmymodel.com: v20140214_110410

--- src/main/java/designPatterns/adapter/AdapterImpl.java
+++ src/main/java/designPatterns/adapter/AdapterImpl.java
@@ -9,6 +9,35 @@ package designPatterns.adapter;
 
 public class AdapterImpl implements Adapter
 {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public Adaptee adaptee;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public AdapterImpl(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	public abstract void request() {
+		// TODO : to implement	
+	}
 	
 	
 }
